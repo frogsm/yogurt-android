543c4dcbcd8567a796fad9cc60d0e5f57186544f
frogsm <frogsm.dev@gmail.com>
2020-10-19 01:50:00 +0900

    코드 정리

--- app/src/main/java/com/frogsm/yogurt/util/BrightnessDelegate.kt
+++ app/src/main/java/com/frogsm/yogurt/util/BrightnessDelegate.kt
@@ -1,5 +1,6 @@
 package com.frogsm.yogurt.util
 
+import android.view.Window
 import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -7,67 +8,59 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 
 interface BrightnessDelegate {
-
     fun initBrightness(fragment: Fragment)
-
-    fun brightnessMax(isMax: Boolean)
-
-    fun brightnessBack()
-
 }
 
 
 class BrightnessDelegateImpl : BrightnessDelegate, LifecycleObserver {
 
-    private var fragment: Fragment? = null
+    private lateinit var fragment: Fragment
+
+    private val window: Window
+        get() = fragment.requireActivity().window
 
     private var brightness = -1f
 
     override fun initBrightness(fragment: Fragment) {
         val fragmentLifecycle = fragment.viewLifecycleOwner.lifecycle
-        val activityWindowAttr = fragment.getActivityWindowAttr()
 
         this.fragment = fragment
-        this.brightness = activityWindowAttr.screenBrightness
+        this.brightness = window.attributes.screenBrightness
 
         // LifecycleObserver 등록
         fragmentLifecycle.addObserver(this)
 
-        brightnessMax(false)
+        brightnessMax(true)
     }
 
 
-    override fun brightnessMax(isMax: Boolean) {
-        fragment?.let {
-
-            val windowAttr = it.getActivityWindowAttr()
-            windowAttr.screenBrightness = if (isMax) {
+    private fun brightnessMax(isMax: Boolean) {
+        with(window.attributes) {
+            screenBrightness = if (isMax) {
                 1f
             } else {
                 0.8f
             }
-
-            it.applyWindowAttr(windowAttr)
+            window.attributes = this
         }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private fun onResume() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private fun onPause() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    override fun brightnessBack() {
-        fragment?.let {
-
-            val windowAttr = it.getActivityWindowAttr()
-            windowAttr.screenBrightness = brightness
-
-            it.applyWindowAttr(windowAttr)
+    private fun onDestroy() {
+        with(window.attributes) {
+            screenBrightness = brightness
+            window.attributes = this
         }
-    }
-
-    private fun Fragment.applyWindowAttr(layoutParams: WindowManager.LayoutParams) {
-        requireActivity().window.attributes = layoutParams
-    }
-
-    private fun Fragment.getActivityWindowAttr(): WindowManager.LayoutParams {
-        return requireActivity().window.attributes
     }
 
 }
